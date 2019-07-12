98a5842280c877168e18e14622ff72467db8526f
EnskrsLab <eneskaras@hotmail.com>
2019-07-12 13:54:34 +0300

    lesson6 finished

--- app/src/main/java/com/example/welcomeactivity/SkillActivity.kt
+++ app/src/main/java/com/example/welcomeactivity/SkillActivity.kt
@@ -1,12 +1,17 @@
 package com.example.welcomeactivity
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class SkillActivity : AppCompatActivity() {
 
+    var league = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+        league = intent.getStringExtra(EXTRA_LEAGUE)
+        println(league)
     }
 }
